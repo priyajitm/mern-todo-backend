fa584b7f1061c97a218b961fb87b0a04cad15cff
Priyajit Mukherjee <priyajit.mukherjee1@gmail.com>
2022-12-25 12:18:49 +0530

    added prod app url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDatabase()
 PassportAuth()
 
 app.use(cors({
-    origin: 'http://localhost:3001'
+    origin: 'https://eloquent-churros-c615ba.netlify.app/'
 }))
 app.use(express.json())
 app.use(session({
